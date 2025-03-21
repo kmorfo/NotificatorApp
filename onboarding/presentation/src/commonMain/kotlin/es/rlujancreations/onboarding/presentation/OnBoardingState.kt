1c0e124c7123803149364fd205d226b72d542666
Raúl Luján <81721889+kmorfo@users.noreply.github.com>
2025-03-21 07:11:13 +0100

    Update OnBoardingState.kt

--- onboarding/presentation/src/commonMain/kotlin/es/rlujancreations/onboarding/presentation/OnBoardingState.kt
+++ onboarding/presentation/src/commonMain/kotlin/es/rlujancreations/onboarding/presentation/OnBoardingState.kt
@@ -1,6 +1,5 @@
 package es.rlujancreations.onboarding.presentation
 
 data class OnBoardingState(
-    val paramOne: String = "default",
-    val paramTwo: List<String> = emptyList(),
+    val hasSeenOnboarding: Boolean = false,
 )
